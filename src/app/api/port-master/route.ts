56cd2c05c37a035d790a03824a37463fea75afaf
rishabh negi <rishabhnegi175@gmail.com>
2025-03-30 10:04:42 +0530

    Fixed tryCatch error parameter

--- src/app/api/port-master/route.ts
+++ src/app/api/port-master/route.ts
@@ -1,16 +1,14 @@
-// import pool from "@/lib/db";
 import { db } from "@/db";
 import { portMaster } from "@/db/schema";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    // const result = await pool.query("SELECT * FROM port_master");
-
     const result = await db.select().from(portMaster);
 
     return NextResponse.json({ data: result }, { status: 200 });
-  } catch {
+  } catch (error) {
+    console.error("Error fetching port data:", error);
     return NextResponse.json(
       { error: "Failed to fetch port data" },
       { status: 500 }
